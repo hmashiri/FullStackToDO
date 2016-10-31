c8647cc6d0181f2298126d0aa751c1e8c988fd8c
Hadjir Mashiri <hadjirmashiri@gmail.com>
2016-10-31 14:45:52 -0700

    fullstack finalized

--- app/MainController.js
+++ app/MainController.js
@@ -1,52 +1,105 @@
 (function() {
-        'use strict';
+    'use strict';
 
-        angular
-            .module('app')
-            .controller('MainController', MainController);
+    angular
+        .module('app')
+        .controller('MainController', MainController);
 
-        MainController.$inject = ['$http', 'ToDoFactory'];
+    MainController.$inject = ['$http', 'ToDoFactory'];
 
-        /* @ngInject */
-        function MainController($http, ToDoFactory) {
-            var vm = this;
-            vm.title = 'MainController';
-            vm.listOftodoItems = [];
-            vm.prioritySelected = '1';
+    /* @ngInject */
+    function MainController($http, ToDoFactory) {
+        var vm = this;
 
-            //activate();
+        vm.TodoItems = [];
+        vm.prioritySelected = '1';
 
-            ////////////////
+        activate();
 
-            vm.addItems = function() {
-                vm.listOftodoItems.push({
-                    priority: parseInt(vm.prioritySelected,10),
-                    description: vm.userTaskDesInput,
-                    title: vm.userTaskNameInput
-                });
+        ////////////////
 
-                var todoItem = {
-                    priority: parseInt(vm.prioritySelected,10),
-                    description: vm.userTaskDesInput,
-                    title: vm.userTaskNameInput
-                };
+        function activate() {
 
-                ToDoFactory.AddTodoToDatabase(todoItem).then(
+            ToDoFactory.getToDo().then(
 
-                    function(response) {
+                function(response) {
+
+                    vm.TodoItems = response;
+
+                }
+
+            );
+        }
 
 
-                    })
 
-                 vm.userTaskNameInput = null;
-                 vm.userTaskDesInput = null;
+        vm.addItems = function() {
+
+            var todoListItems = {
+                todoPriority: parseInt(vm.prioritySelected, 10),
+                todoTaskDescription: vm.userTaskDesInput,
+                todoTaskName: vm.userTaskNameInput
             };
 
+            ToDoFactory.AddTodoToDatabase(todoListItems).then(
 
-        vm.remove = function(todo) {
-            var index = vm.listOftodoItems.indexOf(todo);
-            vm.listOftodoItems.splice(index, 1);
+                function(response) {
+
+                    todoListItems.todoId = response.todoId;
+                    vm.TodoItems.push(todoListItems);
+
+                })
+
+            vm.userTaskNameInput = null;
+            vm.userTaskDesInput = null;
         };
+
+
+        vm.remove = function(TodoId) {
+
+
+            ToDoFactory.RemoveTodoFromDatabase(TodoId).then(
+
+                function(response) {
+
+                    var index = vm.TodoItems.map(function(backend) {
+                        return backend.todoId;
+                    }).indexOf(TodoId);
+
+                    vm.TodoItems.splice(index, 1);
+
+
+                });
+        };
+
+        vm.update = function(id, taskName, taskDescription, priority) {
+
+            var todoListItems = {
+
+                todoId: id,
+                todoTaskName: taskName,
+                todoTaskDescription: taskDescription,
+                todoPriority: parseInt(priority, 10)
+            }
+
+            ToDoFactory.UpdateDatabase(todoListItems).then(
+
+                function(response) {
+
+                    var index = vm.TodoItems.map(function(backend) {
+                        return backend.todoId;
+                    }).indexOf(id);
+
+                    vm.TodoItems[index] = {
+                        todoId: id,
+                        todoTaskName: taskName,
+                        todoTaskDescription: taskDescription,
+                        todoPriority: parseInt(priority, 10)
+                    };
+
+                });
+
+        }
 
 
     }
--- app/ToDoFactory.js
+++ app/ToDoFactory.js
@@ -11,7 +11,9 @@
     function ToDoFactory($http, TodoesAPI, $q) {
         var service = {
             getToDo: getToDo,
-            AddTodoToDatabase: AddTodoToDatabase
+            AddTodoToDatabase: AddTodoToDatabase,
+            RemoveTodoFromDatabase: RemoveTodoFromDatabase,
+            UpdateDatabase: UpdateDatabase,
         };
         return service;
 
@@ -22,50 +24,149 @@
             var defer = $q.defer();
 
             $http({
-                
+
                 method: 'GET',
                 url: TodoesAPI
 
 
-            })
+            }).then(function(response) {
+
+                    if (typeof response.data === 'object') {
+
+                        defer.resolve(response.data);
+
+                    } else {
+
+                        defer.reject(response);
+                    }
+
+                },
+
+                function(error) {
+
+                    defer.reject(error);
+
+                });
+
+            return defer.promise;
         }
 
-        function AddTodoToDatabase(listOftodoItems) {
+
+        function AddTodoToDatabase(todoListItems) {
 
             var defer = $q.defer();
 
             $http({
-                
+
                 method: 'POST',
                 url: TodoesAPI,
                 data: $.param({
-                    TodoTaskName: listOftodoItems.title,
-                    TodoTaskDescription: listOftodoItems.description,
-                    TodoPriority: listOftodoItems.priority
+                    TodoTaskName: todoListItems.todoTaskName,
+                    TodoTaskDescription: todoListItems.todoTaskDescription,
+                    TodoPriority: todoListItems.todoPriority
                 }),
                 headers: {
-                    'Content-Type' : 'application/x-www-form-urlencoded'
+                    'Content-Type': 'application/x-www-form-urlencoded'
 
                 }
-            
-            }).then (function(response) {
 
-                if (typeof response.data === "object") {
+            }).then(function(response) {
 
-                    defer.resolve(response.data);
-                
-                } else {
+                    if (typeof response.data === "object") {
 
-                    defer.reject(response);
-                }
+                        defer.resolve(response.data);
 
-            },
+                    } else {
 
-            function (error) {
+                        defer.reject(response);
+                    }
 
-                defer.reject(error);
-            
-            });
+                },
+
+                function(error) {
+
+                    defer.reject(error);
+
+                });
+
+            return defer.promise;
+        }
+
+
+        function RemoveTodoFromDatabase(TodoId) {
+
+            var defer = $q.defer();
+
+            $http({
+
+                    method: "DELETE",
+                    url: TodoesAPI + TodoId
+                })
+                .then(function(response) {
+
+                        if (typeof response.data === 'object') {
+
+                            defer.resolve(response.data);
+
+                        } else {
+
+                            defer.reject(response);
+
+                        }
+
+                    },
+
+                    function(error) {
+
+                        defer.reject(error);
+
+                    });
+
+            return defer.promise;
+        }
+
+
+
+        function UpdateDatabase(todoListItems) {
+
+            var defer = $q.defer();
+
+            $http({
+
+                    method: "PUT",
+                    url: TodoesAPI + todoListItems.todoId,
+                    data: $.param({
+                        TodoId: todoListItems.todoId,
+                        TodoTaskName: todoListItems.todoTaskName,
+                        TodoTaskDescription: todoListItems.todoTaskDescription,
+                        TodoPriority: todoListItems.todoPriority,
+                    }),
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+
+                    }
+
+                })
+                .then(function(response) {
+
+                        // check the PUT response to see if it was successful or not
+                        if (response.status === 204) {
+
+                            defer.resolve(response.data);
+
+                        } else {
+
+                            defer.reject(response);
+
+                        }
+
+                    },
+                    // error handling of server generated error responses
+                    function(error) {
+
+                        defer.reject(error);
+
+                    });
 
             return defer.promise;
         }
